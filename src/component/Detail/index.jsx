84d30f2027a584f635102eacac6781757394bc3e
Just_Orgil <Onkeyking5@gmail.com>
2025-05-22 21:31:59 +0800

    d

--- src/component/Detail/index.jsx
+++ src/component/Detail/index.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Info } from "../Info";
 import "./style.css";
 
-export const Detail =(props) => {
-    return  <div>
-        <span style={{fontSize: "22.4px"}}>{props.genres}</span><br/>
-        <span style={{fontSize: "16px", color: "#fafafa"}}>{props.prod}</span>
+export const Detail = (props) => {
+    return <div>
+        <span style={{ fontSize: "22.4px" }}>{props.genres}</span><br />
+        <span style={{ fontSize: "16px", color: "#fafafa" }}>{props.prod}</span>
         <div className="Rates">
-        <Info name="Original Release" text={props.relDate}/>
-        <Info name="Box Office" text={`$${parseInt(props.revenue).toLocaleString()}`}/>
-        <Info name="Running Time" text={`${props.runtime} mins`}/>
-        <Info name="Vote Average" text={`${props.voteAvg}/10`}/>
+            <Info name="Original Release" text={props.relDate} />
+            <Info name="Box Office" text={`$${parseInt(props.revenue).toLocaleString()}`} />
+            <Info name="Running Time" text={`${props.runtime} mins`} />
+            <Info name="Vote Average" text={`${props.voteAvg}/10`} />
         </div>
+
     </div>
 }
